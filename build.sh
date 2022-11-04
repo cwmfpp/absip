6de123cd07aa49366bd7ffa819c2bd62b36a3440
wenminchen <wenminchen@126.com>
2022-11-04 08:31:02 +0800

    compiler build

---
+++ build.sh
@@ -0,0 +1,6 @@
+tar zcvf libosip2-5.0.0.tar.gz libosip2-5.0.0 && rm libosip2-5.0.0 -rf && tar xvf libosip2-5.0.0.tar.gz && rm libosip2-5.0.0.tar.gz
+cd libosip2-5.0.0/ && ./configure --prefix=`pwd`/../_install && make && make install-strip
+cd -
+tar zcvf libexosip2-5.0.0.tar.gz libexosip2-5.0.0 && rm libexosip2-5.0.0/ -rf && tar xvf libexosip2-5.0.0.tar.gz && rm libexosip2-5.0.0.tar.gz -rf
+cd libexosip2-5.0.0/ && ./configure --prefix=`pwd`/temp --disable-openssl --enable-shared --includedir=`pwd`/../_install/include libdir=`pwd`/../_install/lib && make && make install-strip
+cd -
